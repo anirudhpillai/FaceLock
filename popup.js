318fe3857f5bd1d2e442b18b7b71757fda9b3344
abdulfaizp <abdulfaizp@gmail.com>
2016-06-25 16:40:28 +0100

    integrated Face API to verify the face (ERROR)

--- popup.js
+++ popup.js
@@ -9,7 +9,7 @@ window.addEventListener("DOMContentLoaded", function() {
     };
 
   // Put video listeners into place
-  if(navigator.getUserMedia) { // Standard
+  if (navigator.getUserMedia) { // Standard
     navigator.getUserMedia(videoObj, function(stream) {
       video.src = stream;
       video.play();
@@ -31,6 +31,41 @@ window.addEventListener("DOMContentLoaded", function() {
     context.drawImage(video, 0, 0, 640, 480);
   });
 
+  document.getElementById("verify").addEventListener("click", function() {
+    var API_KEY = "b067f2e6c5344252b40d9ee0f682fe4f";
+
+    // first get the faceID
+    var params = {
+      "returnFaceId": "true",
+    };
+
+    // get the picture from the canvas before sending it
+    var image  = new Image();
+    var canvas = document.getElementById("canvas");
+    var enc_img = canvas.toDataURL();
+    var enc_img = enc_img.replace(/^data:image\/(png|jpg);base64,/, "");
+
+    $.ajax({
+      url: "https://api.projectoxford.ai/face/v1.0/detect?" + $.param(params),
+      beforeSend: function(xhrObj){
+        // Request headers
+        xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", API_KEY);
+      },
+      type: "POST",
+      // Request body
+
+      data: enc_img,
+    })
+
+    .done(function(data) {
+      alert(data);
+    })
+
+    .fail(function(error) {
+      alert(error);
+    });
+
+  });
+
 }, false);
-
-
