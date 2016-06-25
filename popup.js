8cc1bca112d591391c0d329440e42864db23b396
abdulfaizp <abdulfaizp@gmail.com>
2016-06-25 17:02:45 +0100

    integrated Face API to verify the face (ERROR)

--- popup.js
+++ popup.js
@@ -31,6 +31,28 @@ window.addEventListener("DOMContentLoaded", function() {
     context.drawImage(video, 0, 0, 640, 480);
   });
 
+  makeblob = function (dataURL) {
+            var BASE64_MARKER = ';base64,';
+            if (dataURL.indexOf(BASE64_MARKER) == -1) {
+                var parts = dataURL.split(',');
+                var contentType = parts[0].split(':')[1];
+                var raw = decodeURIComponent(parts[1]);
+                return new Blob([raw], { type: contentType });
+            }
+            var parts = dataURL.split(BASE64_MARKER);
+            var contentType = parts[0].split(':')[1];
+            var raw = window.atob(parts[1]);
+            var rawLength = raw.length;
+
+            var uInt8Array = new Uint8Array(rawLength);
+
+            for (var i = 0; i < rawLength; ++i) {
+                uInt8Array[i] = raw.charCodeAt(i);
+            }
+
+            return new Blob([uInt8Array], { type: contentType });
+        }
+
   document.getElementById("verify").addEventListener("click", function() {
     var API_KEY = "b067f2e6c5344252b40d9ee0f682fe4f";
 
@@ -55,7 +77,7 @@ window.addEventListener("DOMContentLoaded", function() {
       type: "POST",
       // Request body
 
-      data: enc_img,
+      data: makeblob(enc_img),
     })
 
     .done(function(data) {
